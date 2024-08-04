82345cc2b9a2feeb805a9297b934c4a418ea28a5
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2024-08-04 13:23:56 +1000

    fix: readd forEachStaticField

--- specter-base/src/main/java/dev/spiritstudios/specter/api/base/util/ReflectionHelper.java
+++ specter-base/src/main/java/dev/spiritstudios/specter/api/base/util/ReflectionHelper.java
@@ -4,6 +4,7 @@ import org.objectweb.asm.tree.AnnotationNode;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
 
 /**
  * A bunch of utilities to reduce boilerplate reflection code.
@@ -37,6 +38,45 @@ public final class ReflectionHelper {
 		}
 
 		return instance;
+	}
+
+	/**
+	 * Find all static fields of a given type in a class and perform
+	 * an action on them.
+	 *
+	 * @param clazz  Class to search for fields in
+	 * @param target Type to find fields of
+	 * @param action Action to perform on each field
+	 * @param <T>    Type of {@code clazz}
+	 * @param <F>    Type of {@code target}
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T, F> void forEachStaticField(Class<T> clazz, Class<F> target, FieldAction<F> action) {
+		for (Field field : clazz.getDeclaredFields()) {
+			if (!Modifier.isStatic(field.getModifiers())) continue;
+
+			F value;
+			try {
+				value = (F) field.get(null);
+			} catch (IllegalAccessException e) {
+				throw new RuntimeException("Failed to access field " + field.getName(), e);
+			}
+
+			if (value == null) continue;
+			if (!target.isAssignableFrom(value.getClass())) continue;
+
+			action.run(value, field.getName(), field);
+		}
+	}
+
+	/**
+	 * Functional interface for actions to perform on fields.
+	 *
+	 * @param <T> Type of the field
+	 */
+	@FunctionalInterface
+	public interface FieldAction<T> {
+		void run(T value, String name, Field field);
 	}
 
 	/**
