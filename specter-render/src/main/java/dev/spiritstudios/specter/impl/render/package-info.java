30fec1cf7c6b3ebf0d58f496242697c569142a21
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2024-08-20 14:30:07 +1000

    chore: make all `impl` packages internal

---
+++ specter-render/src/main/java/dev/spiritstudios/specter/impl/render/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Internal implementation classes for Specter.
+ * Do not use these classes directly.
+ */
+@ApiStatus.Internal
+package dev.spiritstudios.specter.impl.render;
+
+import org.jetbrains.annotations.ApiStatus;
