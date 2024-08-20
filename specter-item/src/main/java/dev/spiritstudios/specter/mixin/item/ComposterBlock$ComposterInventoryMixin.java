d2121bc387574f5261a4527b3a64eff9f374edc2
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2024-08-20 14:45:20 +1000

    fix: add `remap = false` to some mixins

--- specter-item/src/main/java/dev/spiritstudios/specter/mixin/item/ComposterBlock$ComposterInventoryMixin.java
+++ specter-item/src/main/java/dev/spiritstudios/specter/mixin/item/ComposterBlock$ComposterInventoryMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ComposterBlock.ComposterInventory.class)
 public class ComposterBlock$ComposterInventoryMixin {
-	@WrapOperation(method = "canInsert", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/Object2FloatMap;containsKey(Ljava/lang/Object;)Z"))
+	@WrapOperation(method = "canInsert", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/Object2FloatMap;containsKey(Ljava/lang/Object;)Z", remap = false))
 	private boolean canInsert(Object2FloatMap<ItemConvertible> instance, Object o, Operation<Boolean> original) {
 		ItemConvertible itemConvertible = (ItemConvertible) o;
 		if (SpecterItem.ITEM_TO_LEVEL_INCREASE_CHANCE.containsKey(itemConvertible)) return true;
--- specter-item/src/main/java/dev/spiritstudios/specter/mixin/item/ComposterBlockMixin.java
+++ specter-item/src/main/java/dev/spiritstudios/specter/mixin/item/ComposterBlockMixin.java
@@ -11,15 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ComposterBlock.class)
 public class ComposterBlockMixin {
-	@WrapOperation(method = "onUseWithItem", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/Object2FloatMap;containsKey(Ljava/lang/Object;)Z"))
-	private boolean onUseWithItem(Object2FloatMap<ItemConvertible> instance, Object o, Operation<Boolean> original) {
-		ItemConvertible itemConvertible = (ItemConvertible) o;
-		if (SpecterItem.ITEM_TO_LEVEL_INCREASE_CHANCE.containsKey(itemConvertible)) return true;
-
-		return original.call(instance, o);
-	}
-
-	@WrapOperation(method = "addToComposter", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/Object2FloatMap;getFloat(Ljava/lang/Object;)F"))
+	@WrapOperation(method = "addToComposter", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/Object2FloatMap;getFloat(Ljava/lang/Object;)F", remap = false))
 	private static float addToComposter(Object2FloatMap<ItemConvertible> instance, Object o, Operation<Float> original) {
 		ItemConvertible itemConvertible = (ItemConvertible) o;
 		if (SpecterItem.ITEM_TO_LEVEL_INCREASE_CHANCE.containsKey(itemConvertible))
@@ -28,8 +20,16 @@ public class ComposterBlockMixin {
 		return original.call(instance, o);
 	}
 
-	@WrapOperation(method = "compost", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/Object2FloatMap;containsKey(Ljava/lang/Object;)Z"))
+	@WrapOperation(method = "compost", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/Object2FloatMap;containsKey(Ljava/lang/Object;)Z", remap = false))
 	private static boolean compost(Object2FloatMap<ItemConvertible> instance, Object o, Operation<Boolean> original) {
+		ItemConvertible itemConvertible = (ItemConvertible) o;
+		if (SpecterItem.ITEM_TO_LEVEL_INCREASE_CHANCE.containsKey(itemConvertible)) return true;
+
+		return original.call(instance, o);
+	}
+
+	@WrapOperation(method = "onUseWithItem", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/Object2FloatMap;containsKey(Ljava/lang/Object;)Z", remap = false))
+	private boolean onUseWithItem(Object2FloatMap<ItemConvertible> instance, Object o, Operation<Boolean> original) {
 		ItemConvertible itemConvertible = (ItemConvertible) o;
 		if (SpecterItem.ITEM_TO_LEVEL_INCREASE_CHANCE.containsKey(itemConvertible)) return true;
 
