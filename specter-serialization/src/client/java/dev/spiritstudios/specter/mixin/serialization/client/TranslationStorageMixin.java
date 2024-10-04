b96822543813f95468ddbd4ce31dffc3bb0b0fa3
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2024-10-04 18:56:40 +1000

    chore: mixin cleanup

--- specter-serialization/src/client/java/dev/spiritstudios/specter/mixin/serialization/client/TranslationStorageMixin.java
+++ specter-serialization/src/client/java/dev/spiritstudios/specter/mixin/serialization/client/TranslationStorageMixin.java
@@ -1,5 +1,6 @@
 package dev.spiritstudios.specter.mixin.serialization.client;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
@@ -11,9 +12,7 @@ import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.io.InputStream;
 import java.util.HashMap;
@@ -22,50 +21,55 @@ import java.util.function.BiConsumer;
 
 @Mixin(TranslationStorage.class)
 public class TranslationStorageMixin implements TextTranslationSupplier {
-    @Unique
-    public Map<String, Text> textTranslations;
+	@Unique
+	public Map<String, Text> textTranslations;
 
-    @Redirect(method = "load(Lnet/minecraft/resource/ResourceManager;Ljava/util/List;Z)Lnet/minecraft/client/resource/language/TranslationStorage;", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Maps;newHashMap()Ljava/util/HashMap;", remap = false))
-    private static <K, V> HashMap<K, V> load() {
-        return new TranslationEntryMap<>();
-    }
+	/**
+	 * @author CallMeEcho
+	 * @reason No way to do this without overriding the method
+	 */
+	@Redirect(method = "load(Lnet/minecraft/resource/ResourceManager;Ljava/util/List;Z)Lnet/minecraft/client/resource/language/TranslationStorage;", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Maps;newHashMap()Ljava/util/HashMap;", remap = false))
+	private static <K, V> HashMap<K, V> load() {
+		return new TranslationEntryMap<>();
+	}
 
-    @WrapOperation(method = "load(Lnet/minecraft/resource/ResourceManager;Ljava/util/List;Z)Lnet/minecraft/client/resource/language/TranslationStorage;", at = @At(value = "NEW", target = "(Ljava/util/Map;Z)Lnet/minecraft/client/resource/language/TranslationStorage;"))
-    private static TranslationStorage skipImmutable(Map<String, String> translations, boolean rightToLeft, Operation<TranslationStorage> original, @Local Map<String, String> map) {
-        if (!(map instanceof TranslationEntryMap<String, String> translationEntryMap))
-            return original.call(translations, rightToLeft);
+	@WrapOperation(method = "load(Lnet/minecraft/resource/ResourceManager;Ljava/util/List;Z)Lnet/minecraft/client/resource/language/TranslationStorage;", at = @At(value = "NEW", target = "(Ljava/util/Map;Z)Lnet/minecraft/client/resource/language/TranslationStorage;"))
+	private static TranslationStorage skipImmutable(Map<String, String> translations, boolean rightToLeft, Operation<TranslationStorage> original, @Local Map<String, String> map) {
+		if (!(map instanceof TranslationEntryMap<String, String> translationEntryMap))
+			return original.call(translations, rightToLeft);
 
-        TranslationStorage storage = original.call(translationEntryMap, rightToLeft);
-        ((TranslationStorageMixin) (Object) storage).textTranslations = translationEntryMap.translations();
-        return storage;
-    }
+		TranslationStorage storage = original.call(translationEntryMap, rightToLeft);
+		((TranslationStorageMixin) (Object) storage).textTranslations = translationEntryMap.translations();
+		return storage;
+	}
 
-    @WrapOperation(method = "load(Ljava/lang/String;Ljava/util/List;Ljava/util/Map;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Language;load(Ljava/io/InputStream;Ljava/util/function/BiConsumer;)V"))
-    private static void load(InputStream inputStream, BiConsumer<String, String> entryConsumer, Operation<Void> original, @Local(argsOnly = true) Map<String, String> translations) {
-        if (!(translations instanceof TranslationEntryMap<String, String> translationEntryMap)) {
-            original.call(inputStream, entryConsumer);
-            return;
-        }
+	@WrapOperation(method = "load(Ljava/lang/String;Ljava/util/List;Ljava/util/Map;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Language;load(Ljava/io/InputStream;Ljava/util/function/BiConsumer;)V"))
+	private static void load(InputStream inputStream, BiConsumer<String, String> entryConsumer, Operation<Void> original, @Local(argsOnly = true) Map<String, String> translations) {
+		if (!(translations instanceof TranslationEntryMap<String, String> translationEntryMap)) {
+			original.call(inputStream, entryConsumer);
+			return;
+		}
 
-        original.call(inputStream, new TranslationEntryConsumer(entryConsumer, translationEntryMap::put));
-    }
+		original.call(inputStream, new TranslationEntryConsumer(entryConsumer, translationEntryMap::put));
+	}
 
-    @Inject(method = "hasTranslation", at = @At(value = "HEAD"), cancellable = true)
-    private void hasTranslation(String key, CallbackInfoReturnable<Boolean> cir) {
-        if (textTranslations == null) return;
-        if (textTranslations.containsKey(key)) cir.setReturnValue(true);
-    }
+	@ModifyReturnValue(method = "hasTranslation", at = @At("RETURN"))
+	private boolean hasTranslation(boolean original, @Local(argsOnly = true) String key) {
+		if (textTranslations == null) return original;
+		return original || textTranslations.containsKey(key);
+	}
 
-    @Inject(method = "get", at = @At(value = "HEAD"), cancellable = true)
-    private void get(String key, String fallback, CallbackInfoReturnable<String> cir) {
-        if (textTranslations == null) return;
-        Text text = textTranslations.get(key);
-        if (text != null) cir.setReturnValue(text.getString());
-    }
+	@ModifyReturnValue(method = "get", at = @At("RETURN"))
+	private String get(String original) {
+		if (textTranslations == null) return original;
 
-    @Override
-    public Text specter_serialization$getText(String key) {
-        if (textTranslations == null) return null;
-        return textTranslations.get(key);
-    }
+		Text text = textTranslations.get(original);
+		return text != null ? text.getString() : original;
+	}
+
+	@Override
+	public Text specter_serialization$getText(String key) {
+		if (textTranslations == null) return null;
+		return textTranslations.get(key);
+	}
 }
