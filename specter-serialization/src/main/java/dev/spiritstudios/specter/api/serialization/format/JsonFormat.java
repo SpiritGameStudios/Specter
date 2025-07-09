630bae2cc81d700675cac192505b6a069208eaf2
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2025-07-09 15:11:28 +1000

    chore(serialization): remove usage of deprecated json api

--- specter-serialization/src/main/java/dev/spiritstudios/specter/api/serialization/format/JsonFormat.java
+++ specter-serialization/src/main/java/dev/spiritstudios/specter/api/serialization/format/JsonFormat.java
@@ -3,29 +3,27 @@ package dev.spiritstudios.specter.api.serialization.format;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
+import com.google.gson.Strictness;
 import com.mojang.serialization.JsonOps;
 
 public final class JsonFormat {
 	private static final Gson GSON = new GsonBuilder()
-		.setPrettyPrinting()
-		.setLenient()
-		.create();
-
-	private static final Gson COMPRESSED_GSON = new GsonBuilder()
-		.setLenient()
-		.create();
-
+			.setPrettyPrinting()
+			.setStrictness(Strictness.LENIENT)
+			.create();
 	public static final DynamicFormat<JsonElement> INSTANCE = DynamicFormat.of(
-		JsonOps.INSTANCE,
-		(writer, value) -> GSON.toJson(value, JsonElement.class, writer),
-		reader -> GSON.fromJson(reader, JsonElement.class),
-		"json"
+			JsonOps.INSTANCE,
+			(writer, value) -> GSON.toJson(value, JsonElement.class, writer),
+			reader -> GSON.fromJson(reader, JsonElement.class),
+			"json"
 	);
-
+	private static final Gson COMPRESSED_GSON = new GsonBuilder()
+			.setStrictness(Strictness.LENIENT)
+			.create();
 	public static final DynamicFormat<JsonElement> COMPRESSED = DynamicFormat.of(
-		JsonOps.COMPRESSED,
-		(writer, value) -> COMPRESSED_GSON.toJson(value, JsonElement.class, writer),
-		reader -> COMPRESSED_GSON.fromJson(reader, JsonElement.class),
-		"json"
+			JsonOps.COMPRESSED,
+			(writer, value) -> COMPRESSED_GSON.toJson(value, JsonElement.class, writer),
+			reader -> COMPRESSED_GSON.fromJson(reader, JsonElement.class),
+			"json"
 	);
 }
